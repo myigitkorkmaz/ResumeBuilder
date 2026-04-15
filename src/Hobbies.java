aa9b73543a06e84ce1d6c32942a39403076ba89b
WesternPython24 <111207264+WesternPython24@users.noreply.github.com>
2026-04-15 14:20:24 -0400

    completed Hobbies class code

--- src/Hobbies.java
+++ src/Hobbies.java
@@ -1,2 +1,23 @@
+
+
 public class Hobbies {
+
+    private String description;
+
+    public Hobbies(String description) {
+        this.description = description;
+    }
+
+    public Hobbies() {
+        this.description = "";
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
