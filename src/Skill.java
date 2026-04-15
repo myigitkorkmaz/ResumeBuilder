a1fbe8816e0dd32e91e4d5e1c559db28b063bdfc
WesternPython24 <111207264+WesternPython24@users.noreply.github.com>
2026-04-15 14:01:27 -0400

    Skill class renamed and the class code completed

---
+++ src/Skill.java
@@ -0,0 +1,21 @@
+
+
+public class Skill {
+    private String description;
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill() {
+        this.description = "";
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return description;
+    }
+}
