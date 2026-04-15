3844db4a4c2c4212490b2c1ca54972d345c1ec55
WesternPython24 <111207264+WesternPython24@users.noreply.github.com>
2026-04-15 14:02:35 -0400

    Education class code completed

--- src/Education.java
+++ src/Education.java
@@ -1,2 +1,42 @@
+
 public class Education {
+    private String school;
+    private String graduationYear;
+    private String degree;
+
+    public Education(String school, String graduationYear, String degree) {
+        this.school = school;
+        this.graduationYear = graduationYear;
+        this.degree = degree;
+    }
+
+    public Education() {
+        this.school = "";
+        this.graduationYear = "";
+        this.degree = "";
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public void setGraduatingYear(String graduationYear) {
+        this.graduationYear = graduationYear;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public String getGraduationYear() {
+        return graduationYear;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
 }
